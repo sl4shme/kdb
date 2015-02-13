24df1ad2cd4a00657a4ce541d5efda03c03c4d06
sl4shme <pierre-arthur.mathieu@hp.com>
2015-02-13 19:47:38 +0000

    A voir

--- core.py
+++ core.py
@@ -1,36 +1,50 @@
-class Kdb:
-    def __init__(self):
-        pass
+import shutil
+import os
+# imports
+import db
+import env
 
-    def create(self):
-        pass
 
-    def delete(self):
-        pass
+def create(name, path, tags):
+    shutil.copy2(path, env.path.strip("db.json"))
+    db.addEntry(name, "text", os.path.basename(path), tags)
 
-    def search(self):
-        pass
 
-    def grep(self):
-        pass
+def delete(self):
+     os.remove(path)
 
-    def edit(self):
-        pass
 
-    def git(self):
-        pass
+def search(self):
+    pass
 
-    def exportBkup(self):
-        pass
 
-    def importBkup(self):
-        pass
+def grep(self):
+    pass
 
-    def encrypt(self):
-        pass
 
-    def decrypt(self):
-        pass
+def edit(self):
+    pass
 
-    def get(self):
-        pass
+
+def git(self):
+    pass
+
+
+def exportBkup(self):
+    pass
+
+
+def importBkup(self):
+    pass
+
+
+def encrypt(self):
+    pass
+
+
+def decrypt(self):
+    pass
+
+
+def get(self):
+    pass
--- db.py
+++ db.py
@@ -2,53 +2,53 @@ import json
 import string
 import random
 from datetime import datetime
+import env
 
-PATH = 'db.json'
+
+def readDb():
+    with open(env.path) as f:
+        datas = json.load(f)
+    return datas
 
 
-class Db:
-    def __init__(self):
-        # Add lock
-        # Create if not exist
-        self.readDb()
+def writeDb(newJson):
+    with open(env.path, 'w') as f:
+        json.dump(newJson, f, indent=0)
 
-    def readDb(self):
-        with open(PATH) as f:
-            self.json = json.load(f)
 
-    def writeDb(self):
-        with open(PATH, 'w') as f:
-            json.dump(self.json, f, indent=0)
+def addEntry(name, fileType, path, tags, encrypt=False):
+    uuid = ''.join([random.choice(string.ascii_letters + string.digits)
+                   for n in range(8)])
+    timestamp = str(datetime.now())
+    # datetime.strptime(sstr, "%Y-%m-%d %H:%M:%S.%f")
+    entry = {"uuid": uuid,
+             "name": name,
+             "type": fileType,
+             "path": path,
+             "tags": tags,
+             "encrypt": encrypt,
+             "a_date": timestamp,
+             "m_date": timestamp,
+             "c_date": timestamp}
+    newJson = readDb()
+    newJson.append(entry)
+    writeDb(newJson)
 
-    def addEntry(self, name, fileType, path, tags, encrypt=False):
-        uuid = ''.join([random.choice(string.ascii_letters + string.digits)
-                       for n in range(8)])
-        timestamp = str(datetime.now())
-        # datetime.strptime(sstr, "%Y-%m-%d %H:%M:%S.%f")
-        entry = {"uuid": uuid,
-                 "name": name,
-                 "type": fileType,
-                 "path": path,
-                 "tags": tags,
-                 "encrypt": encrypt,
-                 "a_date": timestamp,
-                 "m_date": timestamp,
-                 "c_date": timestamp}
-        self.json.append(entry)
-        self.writeDb()
 
-    def deleteEntries(self, entries):
-        for entry in entries:
-            self.json.remove(entry)
-        self.writeDb()
+def deleteEntries(entries):
+    newJson = readDb()
+    for entry in entries:
+        newJson.remove(entry)
+    writeDb(newJson)
 
-    def findEntries(self, name="", fileType="", tags=[]):
-        result = self.json
-        if name:
-            result = [item for item in result if name in item['name']]
-        if fileType:
-            result = [item for item in result if fileType in item['type']]
-        if tags:
-            result = [item for item in result if set(tags).issubset(
-                      item['tags'])]
-        return(result)
+
+def findEntries(name="", fileType="", tags=[]):
+    result = readDb()
+    if name:
+        result = [item for item in result if name in item['name']]
+    if fileType:
+        result = [item for item in result if fileType in item['type']]
+    if tags:
+        result = [item for item in result if set(tags).issubset(
+                  item['tags'])]
+    return(result)
